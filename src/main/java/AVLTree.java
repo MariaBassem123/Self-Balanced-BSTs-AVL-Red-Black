51d5a89bca3ad68e43a458df6fb2fe4d73cec222
sara-gaballa <saragaballa2002@gmail.com>
2023-03-18 00:23:30 +0200

    search

--- src/main/java/AVLTree.java
+++ src/main/java/AVLTree.java
@@ -1,9 +1,10 @@
 public class AVLTree implements SelfBalancedBST{
     private AVLNode root;
-
+    private int size = 0;
     @Override
     public boolean insert(Object key) {
         root = BSTInsert(root, (int)key);
+        size += 1;
         return false;
     }
 
@@ -14,12 +15,26 @@ public class AVLTree implements SelfBalancedBST{
 
     @Override
     public boolean search(Object key) {
-        return false;
+        return searchRecursive(root, (int)key);
+    }
+
+    private boolean searchRecursive(AVLNode node, int key) {
+        if (node == null) {
+            return false;
+        }
+        if (node.key == key) {
+            return true;
+        }
+        if (key > node.key) {
+            return searchRecursive(node.right, key);
+        } else {
+            return searchRecursive(node.left, key);
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
@@ -53,20 +68,19 @@ public class AVLTree implements SelfBalancedBST{
     }
     public static void main(String[] args) {
         AVLTree tree = new AVLTree();
-//        tree.root = tree.BSTInsert(tree.root,5);
-//        tree.BSTInsert(tree.root,4);
-//        tree.BSTInsert(tree.root,7);
-//        tree.BSTInsert(tree.root,6);
-//        tree.BSTInsert(tree.root,8);
-//        tree.traverse(tree.root);
-        tree.insert(5);
         tree.insert(4);
+        tree.insert(5);
         tree.insert(7);
         tree.insert(6);
         tree.insert(8);
         tree.insert(1);;
         tree.traverse(tree.root);
-
+        if(tree.search(7)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+        System.out.println(tree.size);
 
     }
 }
