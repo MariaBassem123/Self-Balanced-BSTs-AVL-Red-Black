fe0e3c80883633249498e617dd843798b2790f02
sara-gaballa <saragaballa2002@gmail.com>
2023-03-17 21:37:58 +0200

    Create AVLTree.java

---
+++ src/main/java/AVLTree.java
@@ -0,0 +1,28 @@
+public class AVLTree implements SelfBalancedBST{
+    private AVLNode root;
+    @Override
+    public boolean insert(Object key) {
+
+        return false;
+    }
+
+    @Override
+    public boolean delete(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean search(Object key) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int height() {
+        return 0;
+    }
+}
