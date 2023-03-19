171dfd2644bcd88dd57ced06a685777f7df9b0de
Mariam Hossam <73313364+bangkook@users.noreply.github.com>
2023-03-19 23:15:47 +0200

    Update AVLNode.java

--- src/main/java/AVLNode.java
+++ src/main/java/AVLNode.java
@@ -1,12 +1,14 @@
-public class AVLNode{
-    int key;
-    AVLNode left;
-    AVLNode right;
+public class AVLNode<T> {
+    T key;
+    AVLNode<T> left;
+    AVLNode<T> right;
     int height;
+    boolean marked;
 
-    public AVLNode(int key) {
+    public AVLNode(T key) {
         this.key = key;
         left = right = null;
         height = 1;
+        marked = false;
     }
 }
