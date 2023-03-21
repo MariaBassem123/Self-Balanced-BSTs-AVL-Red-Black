ee67b4881a120e4e8561bf158f6cff88ae40062d
Mariam Hossam <m.hossam2551@gmail.com>
2023-03-21 20:43:52 +0200

    more tests

--- src/test/java/DictionaryTest.java
+++ src/test/java/DictionaryTest.java
@@ -1,8 +1,8 @@
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.awt.*;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -17,16 +17,12 @@ public class DictionaryTest {
     public void testSingleInsert() {
         // test with AVL
         Dictionary dict1 = new Dictionary(new AVLTree<>());
-        boolean insert1 = dict1.insert("Hello");
-        boolean insert2 = dict1.insert("Hello");
-        Assertions.assertTrue(insert1);
-        Assertions.assertFalse(insert2);
+        Assertions.assertTrue(dict1.insert("Hello"));
+        Assertions.assertFalse(dict1.insert("Hello"));
         // test with Red Black
         Dictionary dict2 = new Dictionary(new RedBlackTree<>());
-        boolean insert3 = dict2.insert("Hello");
-        boolean insert4 = dict2.insert("Hello");
-        Assertions.assertTrue(insert3);
-        Assertions.assertFalse(insert4);
+        Assertions.assertTrue(dict2.insert("Hello"));
+        Assertions.assertFalse(dict2.insert("Hello"));
     }
 
     @Test
@@ -39,39 +35,64 @@ public class DictionaryTest {
         testSingleDelete(dict2);
     }
 
-    public void testSingleDelete(Dictionary dict) {
+    private void testSingleDelete(Dictionary dict) {
         dict.insert("Hello");
         dict.insert("World");
         dict.insert("This");
         dict.insert("is");
         dict.insert("me");
-
-        boolean delete1 = dict.delete("me");
-        boolean delete2 = dict.delete("me");
-        boolean delete3 = dict.delete("this");// to show case sensitivity
-        Assertions.assertTrue(delete1);
-        Assertions.assertFalse(delete2);
-        Assertions.assertFalse(delete3);
+        Assertions.assertTrue(dict.delete("me"));
+        Assertions.assertFalse(dict.delete("me"));
+        Assertions.assertFalse(dict.delete("this"));// to show case sensitivity
     }
 
     @Test
-    public void testBatchInsert() throws IOException {
-        ArrayList<String> words = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
+    public void testBatchInsertAndDeleteAVL() {
+        //test with AVL
+        Dictionary dict1 = new Dictionary(new AVLTree<>());
+        Point inserted1 = dict1.batchInsert(rootPath + "\\insert1");
+        Assertions.assertEquals(inserted1.getX(), 17);
+        Assertions.assertEquals(inserted1.getY(), 3);
+        Point deleted1 = dict1.batchDelete(rootPath + "\\delete1");
+        Assertions.assertEquals(deleted1.getX(), 10);
+        Assertions.assertEquals(deleted1.getY(), 10);
+    }
+
+    @Test
+    public void testBatchInsertAndDeleteRB() {
+        // test with Red Black
+        Dictionary dict2 = new Dictionary(new RedBlackTree<>());
+        Point inserted2 = dict2.batchInsert(rootPath + "\\insert1");
+        Assertions.assertEquals(inserted2.getX(), 17);
+        Assertions.assertEquals(inserted2.getY(), 3);
+        Point deleted2 = dict2.batchDelete(rootPath + "\\delete1");
+        Assertions.assertEquals(deleted2.getX(), 10);
+        Assertions.assertEquals(deleted2.getY(), 10);
+    }
+
+    private void generateRandomWords(String fileName, int count) {
+        ArrayList<String> insertWords = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
             try (Stream<String> lines = Files.lines(Paths.get(rootPath + "\\" + "dictionary.txt"))) {
                 int toSkip = rand.nextInt(127142);
                 String line = lines.skip(toSkip).findFirst().get();
-                words.add(line);
+                insertWords.add(line);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        writeFile(words);
+        ArrayList<String> deleteWords = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            int index = rand.nextInt(count - 1);
+            deleteWords.add(insertWords.get(index));
+        }
+        writeFile("insert" + fileName, insertWords);
+        writeFile("delete" + fileName, deleteWords);
     }
 
-    public void writeFile(ArrayList<String> words) {
+    public void writeFile(String fileName, ArrayList<String> words) {
         try {
-            FileWriter writer = new FileWriter(rootPath + "\\" + "insert.txt", true);
+            FileWriter writer = new FileWriter(rootPath + "\\" + fileName, true);
             for (String word : words) {
                 writer.write(word + "\n");
 
@@ -88,6 +109,29 @@ public class DictionaryTest {
 
     }
 
+    @Test
+    public void testSearch() {
+        // test with AVL
+        Dictionary dict1 = new Dictionary(new AVLTree<>());
+        testSearch(dict1);
+        // test with Red Black
+        Dictionary dict2 = new Dictionary(new RedBlackTree<>());
+        testSearch(dict2);
+    }
+
+    private void testSearch(Dictionary dict) {
+        dict.insert("mariam");
+        dict.insert("nancy");
+        dict.insert("eman");
+        dict.insert("maria");
+        dict.insert("sara");
+        Assertions.assertTrue(dict.search("nancy"));
+        Assertions.assertFalse(dict.search("nisreen"));
+        dict.delete("nancy");
+        Assertions.assertFalse(dict.search("nancy"));
+        dict.insert("nisreen");
+        Assertions.assertTrue(dict.search("nisreen"));
+    }
 
     @Test
     public void testSize() {
@@ -133,5 +177,11 @@ public class DictionaryTest {
     @Test
     public void testCombinations() {
 
+    }
+
+    public static void main(String[] args) {
+        DictionaryTest test = new DictionaryTest();
+        test.generateRandomWords("1.txt", 20);
+        test.generateRandomWords("2.txt", 100);
     }
 }
